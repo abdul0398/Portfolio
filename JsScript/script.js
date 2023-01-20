1f8545df7611a990f54a0449e70217fa17d2345a
Abdul_Rehman_Khan <abdulrkhan570@gmail.com>
2023-01-20 10:12:48 +0530

    added responsiveness

--- JsScript/script.js
+++ JsScript/script.js
@@ -1,4 +1,15 @@
+console.log("running...")
 document.querySelector('.cross').style.display = 'none';
 document.querySelector('.hamburger').addEventListener('click', ()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if (document.querySelector('.sidebar').classList.contains('sidebarGo')) {
+        setTimeout(()=>{document.querySelector('.ham').style.display = 'inline';}, 300); 
+        document.querySelector('.cross').style.display = 'none';
+    }else{
+        
+        document.querySelector('.ham').style.display = 'none';
+        setTimeout(()=> {
+            document.querySelector('.cross').style.display = 'inline'}, 350);
+        
+    }
 })
